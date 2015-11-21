75c7693e1bd0cbc3efd1ba5e20a17d1bde92daad
Samuel Gélineau <gelisam+github@gmail.com>
2015-11-20 23:42:22 -0500

    lots of scaffolding

--- main.cpp
+++ main.cpp
@@ -1,50 +1,117 @@
-#include <string.h>
-#include <SDL/SDL.h>
-#include <iostream>
+#include <assert.h>
 #include <emscripten.h>
- 
-using namespace std;
-  
+#include <SDL/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
 SDL_Surface *screen;
- 
-void renderloop() {
-  SDL_Flip(screen);
-  SDL_LockSurface(screen);
-  for(int n=0;n<1000;n++) {
-    int x=rand()%800;
-    int y=rand()%600;
-    int pixel=rand()*100000;
-    int bpp = screen->format->BytesPerPixel;
-    Uint8 *p = (Uint8 *)screen->pixels + y * screen->pitch + x * bpp;
-    if((x>screen->w)||(y>screen->h)||(x<0)||(y<0)) return;
-    *(Uint32 *)p = pixel;
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+
+typedef Uint32 Color;
+
+// Color red = rgb(255,0,0);
+inline Color rgb(Uint8 r, Uint8 g, Uint8 b) {
+  return SDL_MapRGB(screen->format, r, g, b);
+}
+
+// Color red = rgb(0xff0000);
+inline Color rgb(unsigned int rrggbb) {
+  int r = (rrggbb & 0xff0000) >> 16;
+  int g = (rrggbb & 0x00ff00) >> 8;
+  int b = (rrggbb & 0x0000ff);
+  return rgb(r, g, b);
+}
+
+Color current_fg = rgb(0xffffff);
+Color current_bg = rgb(0x000000);
+
+
+double t_at_start, t_at_last_frame, t, dt;
+
+
+struct GameState {
+  double left_paddle_y = 0.5;
+  double right_paddle_y = 0.5;
+  
+  double ball_x = 0.5;
+  double ball_y = 0.5;
+  double ball_dx = 0.5;
+  double ball_dy = 0.5;
+} game_state;
+
+
+void handle_event(const SDL_Event& event) {
+  if(event.key.keysym.sym == SDLK_c) {
+    printf("switch_color\n");
+    current_fg = rgb(0x0000ff);
   }
+}
+
+GameState advance_simulation(const GameState& g) {
+  GameState gg = g;
+  
+  gg.ball_x = g.ball_x + g.ball_dx;
+  gg.ball_y = g.ball_y + g.ball_dy;
+  
+  return gg;
+}
+
+
+inline SDL_Rect rect(double x, double y, double w, double h) {
+  SDL_Rect rect;
+  rect.x = x;
+  rect.y = y;
+  rect.w = w;
+  rect.h = h;
+  return rect;
+}
+
+inline SDL_Rect square(double x, double y, double radius=5) {
+  return rect(x-radius, y-radius, 2*radius, 2*radius);
+}
+
+
+inline void clear(Color color=current_bg) {
+  assert(SDL_FillRect(screen, NULL, color) == 0);
+}
+
+void draw(const SDL_Rect& r, Color color=current_fg) {
+  assert(SDL_FillRect(screen, &r, color) == 0);
+}
+
+void draw_frame() {
+  clear();
+  draw(square(game_state.ball_x, game_state.ball_y));
+}
+
+
+void main_loop() {
+  t = emscripten_get_now() - t_at_start;
+  dt = t - t_at_last_frame;
+  t_at_last_frame = t;
   
   SDL_Event event;
   while(SDL_PollEvent(&event)) {
-    if(event.key.keysym.sym == SDLK_c     ) { SDL_FillRect(screen,NULL,0); }
+    handle_event(event);
   }
   
-  SDL_UnlockSurface(screen);
-  //SDL_Quit();
+  game_state = advance_simulation(game_state);
+  
+  draw_frame();
+  SDL_Flip(screen);
 }
  
 int main() {
- 
+  assert(SDL_Init(SDL_INIT_VIDEO) >= 0);
   
-  if(SDL_Init(SDL_INIT_VIDEO)<0) {
-    cout << "Failed SDL_Init " << SDL_GetError() << endl;
-    return 1;
-  }
+  screen=SDL_SetVideoMode(WIDTH, HEIGHT, 32, SDL_ANYFORMAT);
+  assert(screen);
   
-  screen=SDL_SetVideoMode(800,600,32,SDL_ANYFORMAT);
-  if(screen==NULL) {
-    cout << "Failed SDL_SetVideoMode: " << SDL_GetError() << endl;
-    SDL_Quit();
-    return 1;
-  }
-  
-  emscripten_set_main_loop(renderloop,0,0);
+  t_at_start = emscripten_get_now();
+  emscripten_set_main_loop(main_loop, 0, 0);
   
   return 0;
 }
